5ea6d4a69e1b31184b606aa5d23c2a4fbc8b849f
Lindsay Barrett <labarrett22@gmail.com>
2018-11-20 09:43:24 -0800

    Added Validations

--- guessing_game.py
+++ guessing_game.py
@@ -1,6 +1,7 @@
+
 import random
 
-print("Welcome to the guessing game! ")
+print("***********  Welcome to the guessing game! *********** " )
 
 random_number= random.randint(1, 10) 
 
@@ -9,19 +10,26 @@ def start_game():
     attempt=0
 
     while True:
-        choice=int(input("Guess a number between 1 through 10. "))  
-        if random_number > choice:
-            print("it's higher")
-            attempt+=1
-        elif random_number < choice:            
-            print("it's lower")
-            attempt+=1
-        else:
-            attempt+=1
-            print("You got it! That took",attempt, "attempts. Game Over.")
-            break
+        try:
+            choice=int(input("Guess a number between 1 through 10. "))        
+            if random_number > choice:
+                print("it's higher")
+                attempt+=1
+            elif choice > 10:            
+                print("Your number is to high")
+                attempt+=1
+            elif random_number < choice:            
+                print("it's lower")
+                attempt+=1                     
+            else:
+                attempt+=1
+                print("You got it! That took",attempt, "attempts. Game Over.")
+                break
+        except ValueError as error:
+            print("ERROR. Please insert a numerical number instead")
                      
 start_game()
+
 
 
     
