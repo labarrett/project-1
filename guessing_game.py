843db3c43372feb4041d56394dd2f3485051d303
Lindsay Barrett <labarrett22@gmail.com>
2018-11-20 15:31:19 -0800

    Shortened iteration

--- guessing_game.py
+++ guessing_game.py
@@ -1,3 +1,4 @@
+
 #Guessing game to pick a number between 1-10
 
 import random
@@ -15,19 +16,20 @@ def start_game():
     attempt=0
 
     while True:
+        attempt+= 1
         try:
             choice=int(input("Guess a number between 1 through 10. "))        
             if random_number > choice:
                 print("it's higher")
-                attempt+=1
+                
             elif choice > 10:            
                 print("Your number is to high")
-                attempt+=1
+                
             elif random_number < choice:            
                 print("it's lower")
-                attempt+=1                     
+                                     
             else:
-                attempt+=1
+                   
                 print("You got it! That took",attempt, "attempt(s).")
                 answer=input("Would you look to keep playing? Pick Y/N ")
                 if answer.lower()=="y":
@@ -35,19 +37,10 @@ def start_game():
                 else:
                     print("Thank you for playing!")
                     break
+            
         except ValueError as error:
             print("ERROR. Please insert a numerical number instead")
 
 #run game function
 
-start_game()
-
-
-
-    
-
-    
- 
-
-
-
+start_game()            
