75c404a03157af6fd3bea2e61b69436225163159
Lindsay Barrett <labarrett22@gmail.com>
2018-11-20 09:45:31 -0800

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -1,9 +1,13 @@
+#Guessing game to pick a number between 1-10
 
 import random
 
 print("***********  Welcome to the guessing game! *********** " )
 
 random_number= random.randint(1, 10) 
+
+
+#function of guesing game
 
 def start_game():  
 
@@ -13,21 +17,23 @@ def start_game():
         try:
             choice=int(input("Guess a number between 1 through 10. "))        
             if random_number > choice:
-                print("it's higher")
+                print("It's higher")
                 attempt+=1
             elif choice > 10:            
                 print("Your number is to high")
                 attempt+=1
             elif random_number < choice:            
-                print("it's lower")
+                print("It's lower")
                 attempt+=1                     
             else:
                 attempt+=1
-                print("You got it! That took",attempt, "attempts. Game Over.")
+                print("You got it! That took", attempt, "attempts. Game Over.")
                 break
         except ValueError as error:
             print("ERROR. Please insert a numerical number instead")
-                     
+
+#run game function
+
 start_game()
 
 
