36a7a516b1e351186bb85800a2f35c249479c721
Lindsay <labarrett22@gmail.com>
2018-11-19 20:15:47 -0800

    Create guessing_game.py

---
+++ guessing_game.py
@@ -0,0 +1,35 @@
+import random
+
+print("Welcome to the guessing game! ")
+
+random_number= random.randint(1, 10) 
+
+
+def start_game():  
+
+    attempt=0
+
+    while True:
+        choice=int(input("Guess a number between 1 through 10. "))  
+        if random_number > choice:
+            print("it's higher")
+            attempt+=1
+        elif random_number < choice:            
+            print("it's lower")
+            attempt+=1
+        else:
+            attempt+=1
+            print("You got it! That took",attempt, "attempts. Game Over.")
+            break
+
+                     
+start_game()
+
+
+    
+
+    
+ 
+
+
+
