fef874520cbcf8709e99a53b4f77622ab36a25ad
Lindsay Barrett <labarrett22@gmail.com>
2018-11-20 14:51:42 -0800

    Allowed recursion

--- guessing_game.py
+++ guessing_game.py
@@ -8,11 +8,13 @@ random_number= random.randint(1, 10)
 
 #function of guesing game
 
-def start_game():  
+def start_game():
+
+    random_number= random.randint(1, 10) 
 
     attempt=0
 
-   while True:
+    while True:
         try:
             choice=int(input("Guess a number between 1 through 10. "))        
             if random_number > choice:
@@ -26,10 +28,10 @@ def start_game():
                 attempt+=1                     
             else:
                 attempt+=1
-                print("You got it! That took",attempt, "attempts.")
+                print("You got it! That took",attempt, "attempt(s).")
                 answer=input("Would you look to keep playing? Pick Y/N ")
                 if answer.lower()=="y":
-                    continue
+                    start_game()
                 else:
                     print("Thank you for playing!")
                     break
